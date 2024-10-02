6daea3442ad8b6bbba72cb4c6519dcc11bd7f024
Nagel <pedronagelalves@gmail.com>
2024-10-01 21:42:07 -0300

    teste básico feito no App.js

--- projeto-react/src/App.js
+++ projeto-react/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Exemplo incrivel usando react</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Curabitur accumsan euismod lorem, ut gravida nunc facilisis sit amet.
+        Duis venenatis interdum lectus, at tempor nisi vestibulum sed. 
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        Quisque faucibus, orci vel consectetur efficitur, est metus malesuada libero, 
+        nec vestibulum lorem arcu vel nulla. Integer eget ultricies purus. Morbi posuere, 
+        lacus id tincidunt convallis, lectus elit dictum purus, nec sodales felis lacus vel lorem. 
+        Sed non fringilla eros. Praesent sodales lorem ut malesuada hendrerit. 
+        Proin quis leo quis enim vehicula aliquam.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => alert('Botão testado!')}>
+          Botão teste
+        </button>
       </header>
     </div>
   );
